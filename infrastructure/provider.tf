44e8e062b837a994f75dd33c999b5c7ce6abcc07
gabriel barbosa <gabrielbh91@gmail.com>
2024-02-18 23:22:41 -0300

    fix: removendo variável do terraform backend.

--- infrastructure/provider.tf
+++ infrastructure/provider.tf
@@ -1,17 +1,12 @@
-locals {
-  aws_region = var.aws_region
-}
-
 provider "aws" {
-  region = local.aws_region
+  region = var.aws_region
 }
-
 
 # Centralizar o arquivo de controle de estado do terraform
 terraform {
   backend "s3" {
     bucket = "terraform-state-datalake-dlhdlemr"
     key    = "state/dlhdlemr/terraform.tfstate"
-    region = local.aws_region
+    region = "us-east-2"
   }
 }
